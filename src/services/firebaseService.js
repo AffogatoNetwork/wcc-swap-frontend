449c070a9982437cb048f790507ba7f4d18b4505
jdestephen <jdestephen07@gmail.com>
2020-01-14 15:28:12 -0600

    delete apikey

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -3,7 +3,7 @@ import {db} from '../database/firebase';
 export default class FirebaseDBService {
 
     async addRedeem(transactionHash, data){
-        return db.collection("redeem").doc(transactionHash).set(data)
+        return db.collection("redeems").doc(transactionHash).set(data)
         .then(() => 
             Promise.resolve({status: 'success'})
         )
